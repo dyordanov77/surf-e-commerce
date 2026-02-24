21059ac5de42eb3abf6cfe7650034085fbe6170d
cc231057 <yoda24vj@student.ju.se>
2026-02-24 12:42:24 +0100

    Style the Cart Pop up window

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,6 @@
 // Cart Buttons
 const cartButton = document.getElementById("cartIcon");
 const cart = document.getElementById("cart");
-const closeCarts = document.getElementById("closeCart");
 
 function openCart() {
   cart.style.display = "block";
@@ -11,5 +10,29 @@ function closeCart() {
   cart.style.display = "none";
 }
 
-cartButton.addEventListener("click", openCart);
-closeCarts.addEventListener("click", closeCart);
+cartButton.addEventListener("mouseenter", openCart);
+cart.addEventListener("mouseleave", closeCart);
+
+// Colour Choice
+const colourOptions = document.querySelectorAll(".colour-option");
+
+colourOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    // Remove the selected class from all options
+    colourOptions.forEach((o) => o.classList.remove("selected"));
+
+    // Add the selected class to the clicked option
+    option.classList.add("selected");
+  });
+});
+
+// Size Choice
+const sizeOptions = document.querySelectorAll(".size-option");
+
+sizeOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    sizeOptions.forEach((o) => o.classList.remove("selected"));
+
+    option.classList.add("selected");
+  });
+});
